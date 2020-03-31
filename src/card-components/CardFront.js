41399cf426ee9a4c817242b3fb95b8e3130f80b3
github username <github email address>
2020-03-31 10:39:28 +0100

    Done.

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class CardFront extends Component {
-
   render() {
+    const img = this.props.img;
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
-      </div>
-    )
+      <div
+        className="card-front"
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
+    );
   }
 }
